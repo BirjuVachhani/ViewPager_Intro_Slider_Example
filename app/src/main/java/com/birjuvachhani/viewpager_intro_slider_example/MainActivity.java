c014f91bd5e3c977d998f8b67f06083d8b59c8a7
birju.vachhani <brvachhani@gmail.com>
2018-01-31 15:54:42 +0530

    Chnage AppcompatActivity to Activity

--- app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
@@ -1,12 +1,12 @@
 package com.birjuvachhani.viewpager_intro_slider_example;
 
+import android.app.Activity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private ViewPager viewPager;
 
