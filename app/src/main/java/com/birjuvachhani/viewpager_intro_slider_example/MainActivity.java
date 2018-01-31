6e853e05859793928ba7fc064b32b367066e6aab
birju.vachhani <brvachhani@gmail.com>
2018-01-31 18:17:51 +0530

    Change text color of TitleStrip on page swipe

--- app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
@@ -1,14 +1,18 @@
 package com.birjuvachhani.viewpager_intro_slider_example;
 
 import android.app.Activity;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ViewPager.OnPageChangeListener {
 
     private ViewPager viewPager;
+    private List<DataHolder> mlist = Collections.emptyList();
+    private PagerTitleStrip pagerTitleStrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,28 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
+        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.title_strip);
 
-        List<DataHolder> mlist = new ListGenerator(this).generate();
+        mlist = new ListGenerator(this).generate();
 
         SlideAdapter adapter = new SlideAdapter(this, mlist);
 
         viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        pagerTitleStrip.setTextColor(mlist.get(position).getBackgroundColor());
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
