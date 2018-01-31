11e7e3fd52e3a7abeb4b8d679e2d9f504ee1d6cc
birju.vachhani <brvachhani@gmail.com>
2018-01-31 15:09:07 +0530

    Add ViewPager to activity Generate list of data

--- app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/viewpager_intro_slider_example/MainActivity.java
@@ -1,13 +1,22 @@
 package com.birjuvachhani.viewpager_intro_slider_example;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        
+        List<DataHolder> mlist = new ListGenerator(this).generate();
     }
 }
